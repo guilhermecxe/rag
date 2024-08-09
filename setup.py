11f69151b45fb7dcb0efd4c67615f2986109bb7d
Guilherme Alves <guilhermecxe@hotmail.com>
2024-08-09 16:21:12 -0300

    Added packages and python version necessaries

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 
+# Lendo a lista de pacotes
+with open('requirements.txt', 'r', encoding='utf-16') as f:
+    required = f.read().splitlines()
+
+# Removendo os pacotes de desenvolvimento
+dev_packages = ['pytest']
+required = list(filter(lambda p: not (p.split('==')[0] in dev_packages), required))
+
 setup(
     author="Guilherme Alves",
     description="",
     name="rag",
     version="0.1.0",
     packages=find_packages(include=["rag"]),
+    python_requires='>=3.10',
+    install_requires=required,
 )
